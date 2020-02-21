31d66bba599910b4003a7be470b320e8ac91faf2
Nicolas Miller <njmiller1208@gmail.com>
2020-02-21 10:48:39 -0500

    Renaming folders for better sorting

---
+++ Chapter 2/name.rb
@@ -0,0 +1,9 @@
+# asks user for their name and saves it
+puts "Pleas enter your name: "
+name = gets.chomp
+
+# prints greeting using name
+puts "Hello, #{name}!"
+
+# prints name 10 times
+10.times {puts name}
---
+++ Chapter 3/greeting.rb
@@ -0,0 +1,6 @@
+# takes a name and prints it in a greeting
+def greeting(name)
+  puts "Hello, #{name}!"
+end
+
+greeting("Nick")
